2a6e1d81afa9dc42ee33b93bf4f4938070003f43
Justin Espejo <justin_12_04@yahoo.com>
2016-01-14 19:12:41 -0500

    Initial Commit

---
+++ Sieve AnalysisTests/Sieve_AnalysisTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sieve_AnalysisTests.swift
+//  Sieve AnalysisTests
+//
+//  Created by Justin Espejo on 1/14/16.
+//  Copyright © 2016 Snowcialite. All rights reserved.
+//
+
+import XCTest
+@testable import Sieve_Analysis
+
+class Sieve_AnalysisTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
