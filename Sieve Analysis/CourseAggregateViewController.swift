d7a39ad6ef9b15bef6daf864b4a5f35ad28848e8
Justin Espejo <justin_12_04@yahoo.com>
2016-01-22 20:04:16 -0500

    finished course aggregate

---
+++ Sieve Analysis/CourseAggregateViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  Sieve Analysis
+//
+//  Created by Justin Espejo on 1/15/16.
+//  Copyright © 2016 Snowcialite. All rights reserved.
+//
+
+import UIKit
+
+class CourseAggregateViewController: UIViewController {
+
+    var item1 : String = " "
+    var item2 : String = " "
+    var item3 : String = " "
+    var item4 : String = " "
+    var item5 : String = " "
+
+    var spec1 : String = " "
+    var spec2 : String = " "
+    var spec3 : String = " "
+    var spec4 : String = " "
+    var spec5 : String = " "
+
+    //sieve size label
+    @IBOutlet weak var size1: UILabel!
+    @IBOutlet weak var size2: UILabel!
+    @IBOutlet weak var size3: UILabel!
+    @IBOutlet weak var size4: UILabel!
+    @IBOutlet weak var size5: UILabel!
+    
+    //
+   
+
+    @IBOutlet weak var specLabel5: UILabel!
+    @IBOutlet weak var specLabel4: UILabel!
+    @IBOutlet weak var specLabel3: UILabel!
+    @IBOutlet weak var specLabel2: UILabel!
+    @IBOutlet weak var specLabel1: UILabel!
+    
+    @IBOutlet weak var mass1: UITextField!
+    @IBOutlet weak var mass2: UITextField!
+    @IBOutlet weak var mass3: UITextField!
+    @IBOutlet weak var mass4: UITextField!
+    @IBOutlet weak var mass5: UITextField!
+    @IBOutlet weak var mass6: UITextField!
+    @IBOutlet weak var massTotal: UILabel!
+    
+    
+    @IBOutlet weak var ret1: UILabel!
+    @IBOutlet weak var ret2: UILabel!
+    @IBOutlet weak var ret3: UILabel!
+    @IBOutlet weak var ret4: UILabel!
+    @IBOutlet weak var ret5: UILabel!
+    @IBOutlet weak var ret6: UILabel!
+    @IBOutlet weak var ret7: UILabel!
+    
+    @IBOutlet weak var pass1: UILabel!
+    @IBOutlet weak var pass2: UILabel!
+    @IBOutlet weak var pass3: UILabel!
+    @IBOutlet weak var pass4: UILabel!
+    @IBOutlet weak var pass5: UILabel!
+    
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        
+        size1.text = item1
+        size2.text = item2
+        size3.text = item3
+        size4.text = item4
+        size5.text = item5
+        
+        specLabel1.text = spec1
+        specLabel2.text = spec2
+        specLabel3.text = spec3
+        specLabel4.text = spec4
+        specLabel5.text = spec5
+
+        
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func computePressed(sender: AnyObject)
+    {
+        let passed0 : Double = 100.0
+        if let data1 = Double(mass1.text!){
+            if let data2 = Double(mass2.text!){
+                if let data3 = Double(mass3.text!){
+                    if let data4 = Double(mass4.text!){
+                        if let data5 = Double(mass5.text!){
+                            if let data6 = Double(mass6.text!){
+                                        let total = data1+data2+data3+data4+data5+data6
+                                        let retain1 = data1/total*100
+                                        let passed1 = passed0-retain1
+                                        let retain2 = data2/total*100
+                                        let passed2 = passed1-retain2
+                                        let retain3 = data3/total*100
+                                        let passed3 = passed2-retain3
+                                        let retain4 = data4/total*100
+                                        let passed4 = passed3-retain4
+                                        let retain5 = data5/total*100
+                                        let passed5 = passed4-retain5
+                                        let retain6 = data6/total*100
+                                        let retTotal = retain1+retain2+retain3+retain4+retain5+retain6
+                                
+                                massTotal.text = String(format:"%.2f", total)
+                                
+                                ret1.text = String(format:"%.2f", retain1)
+                                ret2.text = String(format:"%.2f", retain2)
+                                ret3.text = String(format:"%.2f", retain3)
+                                ret4.text = String(format:"%.2f", retain4)
+                                ret5.text = String(format:"%.2f", retain5)
+                                ret6.text = String(format:"%.2f", retain6)
+                                ret7.text = String(format:"%.2f", retTotal)
+                                
+                                
+                                
+                                pass1.text = String(format:"%.2f", passed1)
+                                pass2.text = String(format:"%.2f", passed2)
+                                pass3.text = String(format:"%.2f", passed3)
+                                pass4.text = String(format:"%.2f", passed4)
+                                pass5.text = String(format:"%.2f", passed5)
+
+                                
+
+                                
+
+
+
+                                
+                            }}}}}}
+    }
+}
---
+++ Sieve Analysis/CoursePickerViewController.swift
@@ -0,0 +1,147 @@
+//
+//  CoursePickerViewController.swift
+//  Sieve Analysis
+//
+//  Created by Justin Espejo on 1/22/16.
+//  Copyright © 2016 Snowcialite. All rights reserved.
+//
+
+import UIKit
+
+class CoursePickerViewController: UIViewController {
+
+    @IBAction func pressed67(sender: AnyObject)
+    {
+    
+    }
+
+    @IBAction func pressedState1(sender: AnyObject)
+    {
+    
+    }
+    @IBAction func pressed357(sender: AnyObject)
+    {
+    
+    }
+    @IBAction func pressed57(sender: AnyObject)
+    {
+    
+    }
+    
+    
+    @IBAction func pressed8(sender: AnyObject)
+    {
+    
+    }
+    
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        let vcPass = segue.destinationViewController as! CourseAggregateViewController
+        
+        if(segue.identifier == "seg67"){
+        
+            vcPass.item1 = "1\""
+            vcPass.item2 = "3/4\""
+            vcPass.item3 = "3/8\""
+            vcPass.item4 = "#4"
+            vcPass.item5 = "#8"
+            
+            vcPass.spec1 = "100"
+            vcPass.spec2 = "90-100"
+            vcPass.spec3 = "20-55"
+            vcPass.spec4 = "0-10"
+            vcPass.spec5 = "0-5"
+
+
+
+            
+        }
+        
+        else if(segue.identifier == "seg57"){
+            
+            vcPass.item1 = "1 1/2\""
+            vcPass.item2 = "1\""
+            vcPass.item3 = "1/2\""
+            vcPass.item4 = "#4"
+            vcPass.item5 = "#8"
+            
+            vcPass.spec1 = "100"
+            vcPass.spec2 = "95-100"
+            vcPass.spec3 = "25-60"
+            vcPass.spec4 = "0-10"
+            vcPass.spec5 = "0-5"
+            
+        }
+        
+        else if(segue.identifier == "seg357"){
+            vcPass.item1 = "2 1/2\""
+            vcPass.item2 = "2\""
+            vcPass.item3 = "1\""
+            vcPass.item4 = "1/2\""
+            vcPass.item5 = "#4"
+            
+            vcPass.spec1 = "100"
+            vcPass.spec2 = "95-100"
+            vcPass.spec3 = "35-70"
+            vcPass.spec4 = "10-30"
+            vcPass.spec5 = "0-5"
+        }
+        
+        else if(segue.identifier == "seg8"){
+            vcPass.item1 = "1/2\""
+            vcPass.item2 = "3/8\""
+            vcPass.item3 = "#4"
+            vcPass.item4 = "#8"
+            vcPass.item5 = "#16"
+            
+            vcPass.spec1 = "100"
+            vcPass.spec2 = "85-100"
+            vcPass.spec3 = "10-30"
+            vcPass.spec4 = "0-10"
+            vcPass.spec5 = "0-5"
+        }
+        
+        else {
+            vcPass.item1 = "1 1/2\""
+            vcPass.item2 = "1\""
+            vcPass.item3 = "1/2\""
+            vcPass.item4 = "1/4\""
+            vcPass.item5 = " "
+
+            vcPass.spec1 = "100"
+            vcPass.spec2 = "93-100"
+            vcPass.spec3 = "27-58"
+            vcPass.spec4 = "0-8"
+            vcPass.spec5 = " "
+            
+        }
+        
+    }
+    
+    
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
